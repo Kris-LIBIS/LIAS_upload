3c285b4f61376145f3276f3757f5b6e569dcc404
Kris Dekeyser <kris.dekeyser@libis.be>
2012-04-18 17:58:28 +0200

    Rails application created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LIASUpload::Application.config.secret_token = '8d1547d4d4bf4dff2e2d07cdc75eec257bb95b55b698d97318a9e46db9be29ba5781e6afe29c1e9b845935475ea64014042d560c1c02e379f0aafcfe1db5fdc9'
